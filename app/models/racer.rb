fa0bd84a3f46fb96cce5d88a94f2b2f658bdf90d
Igor Damasceno <igor.damasceno@codeminer42.com>
2023-02-23 15:12:22 -0300

    feat: add racer model

---
+++ app/models/racer.rb
@@ -0,0 +1,8 @@
+class Racer < ApplicationRecord
+
+  MIN_AGE = 18
+
+  validates :name, presence: true
+  validates :born_at, presence: true, comparison: { less_than_or_equal_to: MIN_AGE.years.ago, message: "must be at least #{MIN_AGE} years old" }
+  validates :image_url, format: { with: URI::DEFAULT_PARSER.make_regexp }
+end
---
+++ spec/models/racer_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Racer, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+
+    it { should validate_presence_of :born_at }
+    it { should allow_value((Racer::MIN_AGE + 1).years.ago).for :born_at }
+    it { should_not allow_value((Racer::MIN_AGE - 1).years.ago).for :born_at }
+
+    it { should allow_value(Faker::Internet.url).for :image_url }
+    it { should_not allow_value(Faker::File.file_name).for :image_url }
+  end
+end
