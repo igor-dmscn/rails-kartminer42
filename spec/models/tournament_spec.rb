de192dde93de3a3877ab177f9b4979a3a0a8ce43
Igor Damasceno <igor.damasceno@codeminer42.com>
2023-02-23 17:33:02 -0300

    feat: add races association to tournament

--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Tournament, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
+
+  describe 'associations' do
+    it { should have_many :races }
+  end
 end
