00653bb4a918e8b0eca09c580e66f6ebe3af27c8
Igor Damasceno <igor.damasceno@codeminer42.com>
2023-02-23 15:35:50 -0300

    feat: add tournament model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_23_182246) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_182753) do
   create_table "racers", force: :cascade do |t|
     t.string "name"
     t.date "born_at"
@@ -22,6 +22,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_182246) do
   create_table "races", force: :cascade do |t|
     t.datetime "date"
     t.string "place"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tournaments", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
