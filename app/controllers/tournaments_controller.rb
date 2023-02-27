d2fdfe7d0e7e889742d649fea920702b959dd8b4
Igor Damasceno <igor.damasceno@codeminer42.com>
2023-02-27 11:36:08 -0300

    feat: add tournament controller create method

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,2 +1,16 @@
 class TournamentsController < ApplicationController
+  def create
+    @tournament = Tournament.new(tournament_params)
+
+    if @tournament.save
+      head :created
+    else
+      head :bad_request
+    end
+  end
+
+  private
+  def tournament_params
+    params.required(:tournament).permit(:name)
+  end
 end
--- spec/controllers/tournaments_controller_spec.rb
+++ spec/controllers/tournaments_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe TournamentsController, type: :controller do
+  describe 'POST create' do
+    context 'with valid name param' do
+      let(:request_body) do
+        {
+          name: 'Tournament'
+        }
+      end
 
+      it 'returns status created' do
+        post :create, params: { tournament: request_body }, format: :json
+
+        expect(response).to be_created
+      end
+    end
+
+    context 'with invalid name param' do
+      let(:request_body) do
+        {
+          name: nil
+        }
+      end
+
+      it 'returns status bad request' do
+        post :create, params: { tournament: request_body }, format: :json
+
+        expect(response).to be_bad_request
+      end
+    end
+  end
 end
