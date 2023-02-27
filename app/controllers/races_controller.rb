a26b04dadf5bfceeabb25d85981d667e5a6590d5
Igor Damasceno <igor.damasceno@codeminer42.com>
2023-02-27 10:40:07 -0300

    feat: race controller show method

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,5 +1,15 @@
 class RacesController < ApplicationController
+  before_action :find_race, only: [:show]
   def index
     @races = Race.all.includes(:placements).order('placements.position ASC')
+  end
+
+  def show;  end
+
+  private
+  def find_race
+    @race = Race.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: true, message: 'Not found' }, status: :not_found
   end
 end
